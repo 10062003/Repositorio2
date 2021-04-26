0d8445fcc1e6176088d1ce0d901b90eb777bd7d2
Hogar <Hogar@Hogar-PC>
2021-04-25 22:08:30 -0500

    Mi aporte en el trabajo es el Hola DaniMundo en Hola2.java

---
+++ src/hola2/Hola2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola2;
+
+/**
+ *
+ * @author Hogar
+ */
+public class Hola2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Hola DaniMundo");
+        
+    }
+    
+}
